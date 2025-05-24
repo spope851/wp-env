030c92639f09c5ebf4dc5ee288def8db4220b4d2
Spencer Pope <spope851@gmail.com>
2025-05-24 11:53:34 +0800

    add offline dev config lines to wp-config.php example

--- examples/wp-config.php
+++ examples/wp-config.php
@@ -19,8 +19,6 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV["WP_DATABASE"] );
 
 /** Database username */
 define( 'DB_USER', $_ENV["WP_DATABASE_USER"] );
@@ -29,16 +27,26 @@ define( 'DB_USER', $_ENV["WP_DATABASE_USER"] );
 define( 'DB_PASSWORD', $_ENV["WP_DATABASE_PASSWORD"] );
 
 /** Database hostname */
-define( 'DB_HOST', $_ENV["WP_DATABASE_HOST"] );
-
-/** Database port */
-define( 'DB_PORT', $_ENV["WP_DATABASE_PORT"] );
+define( 'DB_HOST', $_ENV["WP_DATABASE_HOST"] . ":" . $_ENV["WP_DATABASE_PORT"] );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+// depending on your environment
+define('WP_HOME', "http://localhost:8080/");
+define('WP_SITEURL', "http://localhost:8080/");
+/** The name of the database for WordPress */
+define('DB_NAME', $_ENV["WP_DATABASE"]); // best to keep it the same as production
+/* Disable WP Cron, we're handling this via cPanel instead */
+define('DISABLE_WP_CRON', true);
+define('WP_POST_REVISIONS', 5);
+/* Disable the Theme & Plugin Editor */
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'WP_CACHE', 'false' ); // Added by WP Rocket
+
 
 /**#@+
  * Authentication unique keys and salts.
@@ -70,7 +78,8 @@ define( 'WP_ENVIRONMENT_TYPE', 'local' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+// $table_prefix = 'wp_';
+$table_prefix = 'dfs_'; // best to keep it the same as production
 
 /**
  * For developers: WordPress debugging mode.
@@ -84,12 +93,27 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', true );
+
+error_reporting(E_ALL & ~E_DEPRECATED)
+/* uncomment for testing */
+;define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
+define('WP_DEBUG_DISPLAY', false); // Prevents errors from showing on the site
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+// Prevent WordPress from checking for updates
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define( 'WP_AUTO_UPDATE_CORE', false );
 
+// For offline development
+// Prevent WordPress from making external HTTP requests
+define( 'WP_HTTP_BLOCK_EXTERNAL', true );
+define( 'WP_ACCESSIBLE_HOSTS', '' );
+
+// Disable WordPress.org API calls
+define( 'WP_DISABLE_CRON', true );
+define( 'DISABLE_WP_CRON', true );
 
 /* That's all, stop editing! Happy publishing. */
 
